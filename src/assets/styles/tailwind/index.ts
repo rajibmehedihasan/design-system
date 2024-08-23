1c97f91ecc8c3be3431daeae6c25bada62b40d5f
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2024-08-24 02:12:02 +0600

    update variables and typography

--- src/assets/styles/tailwind/index.ts
+++ src/assets/styles/tailwind/index.ts
@@ -1,10 +1,13 @@
 import colorVariables from "./colors";
 import spacingVariables from "./spacing";
 import { borderWidthVariables, borderRadiusVariables } from "./border";
+import { fontSizeVariables, lineHeightVariables } from "./typography";
 
 export {
     colorVariables,
     spacingVariables,
     borderWidthVariables,
     borderRadiusVariables,
+    fontSizeVariables,
+    lineHeightVariables,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,21 @@ import {
     spacingVariables,
     borderWidthVariables,
     borderRadiusVariables,
+    fontSizeVariables,
+    lineHeightVariables,
 } from "./src/assets/styles/tailwind";
 
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        fontSize: {
-            base: "15px",
-        },
         extend: {
             fontFamily: {
                 sans: ["var(--font-family-sans)"],
                 serif: ["var(--font-family-serif)"],
                 mono: ["var(--font-family-mono)"],
+            },
+            fontSize: {
+                ...fontSizeVariables,
             },
             colors: {
                 ...colorVariables,
@@ -34,7 +36,26 @@ export default {
             borderRadius: {
                 ...borderRadiusVariables,
             },
+            lineHeight: {
+                ...lineHeightVariables,
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        function ({ addUtilities }) {
+            const newUtilities = {
+                ".text-html": {
+                    fontSize: "var(--font-size-html)", // Custom font size using CSS variable
+                },
+                ".text-body": {
+                    fontSize: "var(--font-size-body)", // Custom font size using CSS variable
+                },
+                ".leading-base": {
+                    lineHeight: "var(--line-height-base)",
+                },
+            };
+
+            addUtilities(newUtilities);
+        },
+    ],
 };
